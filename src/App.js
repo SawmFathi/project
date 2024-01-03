1d28b7a492cb52cd7dc968c10b107a13b8e9b4b9
SawmFathi <samfathi2004@gmail.com>
2024-01-03 17:58:45 -0500

    myfirstcommit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       {/* add image here */}
+        <h1>Welcome to Sam Fathi's Personal Website</h1>
+        <p>
+          Hello, I'm Sam Fathi. This is my personal website.
+        </p>
+        <p>
+          Here you can find information about me, my projects, and more.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
